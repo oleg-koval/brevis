4b11334084babc94626f49c708d35c4ba5ea9a11
Ed <kvl.olg@gmail.com>
2020-02-20 12:14:34 +0100

    ci: setup basic CI (#4)
    Closes #3

--- src/handle.ts
+++ src/handle.ts
@@ -2,20 +2,38 @@ import { Handler, APIGatewayEvent, Context } from 'aws-lambda';
 
 // import { logInfo } from './logging/index';
 
+enum Paths {
+  SayHello = '/say-hello',
+  Messages = '/messages',
+}
+
 export const hello: Handler = async (
   event: APIGatewayEvent,
   context: Context,
   // eslint-disable-next-line @typescript-eslint/require-await
 ) => {
-  return {
+  const response = {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Test',
         input: { event, context },
       },
       null,
       2,
     ),
   };
+  const responseTwo = {
+    statusCode: 500,
+    body: JSON.stringify(
+      {
+        message: 'Bad',
+        // input: { event, context },
+      },
+      null,
+      2,
+    ),
+  };
+
+  return event.path === Paths.SayHello ? response : responseTwo;
 };
