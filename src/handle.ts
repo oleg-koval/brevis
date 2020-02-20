9b07ef78591bf06c96c086c8b1203f25699eaaca
Ed <kvl.olg@gmail.com>
2020-02-20 13:14:40 +0100

    feat: update endpoints with dummy data

--- src/handle.ts
+++ src/handle.ts
@@ -1,39 +1,66 @@
+/* eslint-disable functional/no-conditional-statement */
+/* eslint-disable functional/no-expression-statement */
 import { Handler, APIGatewayEvent, Context } from 'aws-lambda';
 
-// import { logInfo } from './logging/index';
+import { logInfo } from './logging/index';
 
 enum Paths {
   SayHello = '/say-hello',
   Messages = '/messages',
 }
 
+const GetPayload = {
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Hello, World!',
+    },
+    null,
+    2,
+  ),
+};
+
+const PostPayload = {
+  statusCode: 201,
+  body: JSON.stringify(
+    {
+      message: 'Created.',
+    },
+    null,
+    2,
+  ),
+};
+
+const getResponse = (
+  path: Paths,
+): { readonly statusCode: number; readonly body: string } => {
+  switch (path) {
+    case Paths.SayHello:
+      return GetPayload;
+    case Paths.Messages:
+      return PostPayload;
+    default:
+      'wrong path';
+  }
+
+  return {
+    statusCode: 403,
+    body: JSON.stringify(
+      {
+        message: 'Bad request.',
+      },
+      null,
+      2,
+    ),
+  };
+};
+
 export const hello: Handler = async (
   event: APIGatewayEvent,
   context: Context,
   // eslint-disable-next-line @typescript-eslint/require-await
 ) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Test',
-        input: { event, context },
-      },
-      null,
-      2,
-    ),
-  };
-  const responseTwo = {
-    statusCode: 500,
-    body: JSON.stringify(
-      {
-        message: 'Bad',
-        // input: { event, context },
-      },
-      null,
-      2,
-    ),
-  };
+  logInfo(JSON.stringify(context));
 
-  return event.path === Paths.SayHello ? response : responseTwo;
+  return getResponse(event.path as Paths);
 };
